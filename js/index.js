b250d490280782abc7e51c126c0e1afc0c41586a
Alessandra Perricone <alessandraperricone3@gmail.com>
2021-12-03 12:34:26 +0100

    App quasi definita

---
+++ js/index.js
@@ -0,0 +1,176 @@
+import {cities, lang, traductionText} from "./utils.js";
+const inputSelect = document.querySelector(".inputSelect");
+const citySelect = document.querySelector("#selectCity");
+const render = (container, content) => (container.innerHTML = content);
+
+
+//I ADD OPTION VALUES TO THE CITY SELECT ELEMENT FROM ARRAY CITIES ON UTILS.JS
+
+function loadCity() {
+    selectCity.innerHTML = cities.reduce((options, {name,value})=>
+    options+=`<option value="${value}">${name}</option>`);
+}
+
+document.addEventListener("DOMContentLoaded", loadCity);
+
+
+
+//I ADD OPTION VALUES TO THE LANGUAGE SELECT ELEMENT FROM ARRAY LANG ON UTILS.JS
+function loadLanguage() {
+    selectLang.innerHTML = lang.reduce((options, {name,value})=>
+    options+=`<option value="${value}">${name}</option>"`);
+}
+
+
+
+//AT THE CHANGE EVENT OF THE SELECT (CITY) I APPEND ANOTHER SELECT TO CHOOSE THE LANGUAGE
+citySelect.addEventListener('change', ()=>{
+    const selectLang = document.createElement("select");
+    selectLang.setAttribute("id", "selectLang");
+    inputSelect.appendChild(selectLang);
+
+    // DISABLED THE FIRT SELECT ELEMENT (CITY), SO THE USER CANNOT APPEND ANOTHER LANGUAGE SELECT
+    citySelect.disabled = true;
+
+    loadLanguage();
+
+    //I TAKE THE CITY VALUE THAT THE USER HAS ENTERED
+    let cityValue = citySelect.value;
+    
+    // I DECLARE THE LANG SELECT ELEMENT AND TAKE THE LANG VALUE THAT USER HAS ENTERED
+    const selectLangInput = document.querySelector("#selectLang");
+    selectLangInput.addEventListener('change', () =>{
+        let langValue = selectLangInput.value;
+
+        // I CONCAT THE ELEMENTS OF API URL
+        let apiUrl ='https://api.openweathermap.org/data/2.5/weather?q='+ cityValue +'&appid=9b382578f78572b78aaeb4a31933a917&lang=' + langValue;
+        
+        // I ACTIVE THE ELEMET TO SELECT THE CITY, SO IF USER WANT IT CAN DO ANOTHER RESEARCH
+        citySelect.disabled = false;
+        
+        // REMOVE THE LANG SELECT, IT WILL APPEAR WHEN USER CHOOSE THE CITY
+        inputSelect.removeChild(selectLangInput);
+        
+        // I SET THE LOCAL STORAGE TO HOLD LANGUAGE VALUE
+        localStorage.setItem('lang', langValue);
+
+        // I SET TRY TO HANDLING ERRORS 
+        try {
+
+        // I DO A FETCH TO TAKE THE DATA FROM EXTERNAL SERVER
+        fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => list(data));
+
+        //I DECLARE THE VALUE WHERE I'LL FIND THE LANG VALUE OF LOCAL STORAGE
+        const getLang = localStorage.getItem('lang');
+
+
+// I DO A SWITCH TO DIFFERENCE THE LANGUAGE AND TO RENDER IN DIFFERENT LANGUAGE WITH array.MAP AND THE TEMPLATE STRING
+// I DECLARE A NER ARRAY BECAUSE .MAP IS AN ARRAY METHOD, DATA IS AN OBJECT. I PUSH DATA INTO ARRAYDATA TO DO A .MAP.
+switch (getLang){
+    case "it":
+        var list = (data) => {
+            let arrayData = [];
+            arrayData.push(data);
+            const elements = arrayData
+            .map(item =>`<h3>${item.name}</h3>
+            <sup>${item.coord.lat}°N ${item.coord.lon}°E</sup>
+            <p><img src="https://img.icons8.com/plasticine/100/000000/apple-weather.png"/>Il meteo oggi prevede ${item.weather[0].description}.</p>
+            <p><img src="https://img.icons8.com/clouds/100/000000/storm.png"/>Le nuvole sono al ${item.clouds.all}%.</p>
+            <p><img src="https://img.icons8.com/clouds/100/000000/windsock.png"/>La velocità del vento è ${item.wind.speed}m/s</p>
+            `)
+            .join ('');
+        
+
+            const container = document.querySelector(".container");
+        
+            render(container,
+                `<h2 id="subtitle">Your weather forecast today!</h2>
+                <div id="cardWeather">${elements}</div>`);
+            }
+        break;
+
+    case "en":
+        var list = (data) => {
+            let arrayData = [];
+            arrayData.push(data);
+            // console.log(arrayData);
+            const elements = arrayData
+            .map(item =>`<h3>${item.name}</h3>
+            <sup>${item.coord.lat}°N ${item.coord.lon}°E</sup>
+            <p><img src="https://img.icons8.com/plasticine/100/000000/apple-weather.png"/>The weather forecast today ${item.weather[0].description}.</p>
+            <p><img src="https://img.icons8.com/clouds/100/000000/storm.png"/>The clouds are at ${item.clouds.all}%.</p>
+            <p><img src="https://img.icons8.com/clouds/100/000000/windsock.png"/>Wind speed is ${item.wind.speed}m/s</p>
+            `)
+            .join ('');
+        
+            const container = document.querySelector(".container");
+        
+            render(container,
+                `<h2 id="subtitle">Your weather forecast today!</h2>
+                <div id="cardWeather">${elements}</div>`);
+        }
+        break;
+
+        case "fr":
+            var list = (data) => {
+                let arrayData = [];
+                arrayData.push(data);
+                // console.log(arrayData);
+                const elements = arrayData
+                .map(item =>`<h3>${item.name}</h3>
+                <sup>${item.coord.lat}°N ${item.coord.lon}°E</sup>
+                <p><img src="https://img.icons8.com/plasticine/100/000000/apple-weather.png"/>La météo d'aujourd'hui ${item.weather[0].description}.</p>
+                <p><img src="https://img.icons8.com/clouds/100/000000/storm.png"/>Les nuages ​​sont al ${item.clouds.all}%.</p>
+                <p><img src="https://img.icons8.com/clouds/100/000000/windsock.png"/>La vitesse du vent est ${item.wind.speed}m/s</p>
+                `)
+                .join ('');
+            
+                const container = document.querySelector(".container");
+            
+                render(container,
+                    `<h2 id="subtitle">Your weather forecast today!</h2>
+                    <div id="cardWeather">${elements}</div>`);
+            }
+            break;
+
+    case "es":
+        var list = (data) => {
+            let arrayData = [];
+            arrayData.push(data);
+            // console.log(arrayData);
+            const elements = arrayData
+            .map(item =>`<h3>${item.name}</h3>
+            <sup>${item.coord.lat}°N ${item.coord.lon}°E</sup>
+            <p><img src="https://img.icons8.com/plasticine/100/000000/apple-weather.png"/>El pronóstico del tiempo hoy ${item.weather[0].description}.</p>
+            <p><img src="https://img.icons8.com/clouds/100/000000/storm.png"/>Las nubes son todas ${item.clouds.all}%.</p>
+            <p><img src="https://img.icons8.com/clouds/100/000000/windsock.png"/>La velocidad del viento es ${item.wind.speed}m/s</p>
+            `)
+            .join ('');
+        
+            const container = document.querySelector(".container");
+        
+            render(container,
+                `<h2 id="subtitle">Your weather forecast today!</h2>
+                <div id="cardWeather">${elements}</div>`);
+        }
+        break;
+};
+
+// I DEFINE THE STATEMENT THROW (FOR TRY AND CATCH)
+if (cityValue == false || cityValue == null ) { throw "city"; }
+if (langValue == false || langValue == null ) { throw "language"; }
+}
+// I CATCH THE ERROR AND I DEFINE WHAT TO DO IN CASE OF ERROR
+catch(err) {
+         alert ("Please enter your favorite " + err + "!");
+}
+    });
+ 
+});
+
+
+
+
+
---
+++ js/utils.js
@@ -0,0 +1,403 @@
+
+const cities = [
+    {
+        name:"",
+        value:""
+    },
+    {
+        name:"Enter your favorite city!",
+        value:"Enter your favorite city!"
+    },
+    {
+        name:"",
+        value:""
+    },
+    {
+        name: "Palermo",
+        value: "Palermo"
+    },
+    {
+        name: "Bagheria",
+        value: "bagheria"
+    },
+    {
+        name: "Monreale",
+        value: "monreale"
+    },
+    {
+        name: "Carini",
+        value: "carini"
+    },
+    {
+        name: "Partinico",
+        value: "partinico"
+    },
+    {
+        name: "Misilmeri",
+        value: "misilmeri"
+    },
+    {
+        name: "Termini Imerese",
+        value: "termini+imerese"
+    },
+    {
+        name: "Villabate",
+        value: "villabate"
+    },
+    {
+        name: "Cefalù",
+        value: "cefalù"
+    },
+    {
+        name: "Ficarazzi",
+        value: "ficarazzi"
+    },
+    {
+        name: "Cinisi",
+        value: "cinisi"
+    },
+    {
+        name: "Terrasini",
+        value: "terrasini"
+    },
+    {
+        name: "Casteldaccia",
+        value: "casteldaccia"
+    },
+    {
+        name: "Capaci",
+        value: "capaci"
+    },
+    {
+        name: "Belmonte Mezzagno",
+        value: "belmonte+mezzagno"
+    },
+    {
+        name: "Santa Flavia",
+        value: "santa+flavia"
+    },
+    {
+        name: "Corleone",
+        value: "corleone"
+    },
+    {
+        name: "Trabia",
+        value: "trabia"
+    },
+    {
+        name: "Altofonte",
+        value: "altofonte"
+    },
+    {
+        name: "Castelbuono",
+        value: "castelbuono"
+    },
+    {
+        name: "San Giuseppe Jato",
+        value: "san+giuseppe+jato"
+    },
+    {
+        name: "Altavilla Milicia",
+        value: "altavilla+milicia"
+    },
+    {
+        name: "Caccamo",
+        value: "caccamo"
+    },
+    {
+        name: "Campofelice di Roccella",
+        value: "campofelice+di+roccella"
+    },
+    {
+        name: "Borgetto",
+        value: "borgetto"
+    },
+    {
+        name: "Isola delle femmine",
+        value: "isola+delle+femmine"
+    },
+    {
+        name: "Lercara Friddi",
+        value: "lercara+friddi"
+    },
+    {
+        name: "Gangi",
+        value: "gangi"
+    },
+    {
+        name: "Marineo",
+        value: "marineo"
+    },
+    {
+        name: "Balestrate",
+        value: "balestrate"
+    },
+    {
+        name: "Piana degli Albanesi",
+        value: "piana+degli+albanesi"
+    },
+    {
+        name: "Montelepre",
+        value: "montelepre"
+    },
+    {
+        name: "San Cipirello",
+        value: "san+cipirello"
+    },
+    {
+        name: "Cerda",
+        value: "cerda"
+    },
+    {
+        name: "Prizzi",
+        value: "prizzi"
+    },
+    {
+        name: "Bisacquino",
+        value: "bisacquino"
+    },
+    {
+        name: "Torretta",
+        value: "torretta"
+    },
+    {
+        name: "Bolognetta",
+        value: "bolognetta"
+    },
+    {
+        name: "Collesano",
+        value: "collesano"
+    },
+    {
+        name: "Caltavuturo",
+        value: "caltavuturo"
+    },
+    {
+        name: "Ciminna",
+        value: "ciminna"
+    },
+    {
+        name: "Lascari",
+        value: "lascari"
+    },
+    {
+        name: "Alia",
+        value: "alia"
+    },
+    {
+        name: "Valledolmo",
+        value: "valledolmo"
+    },
+    {
+        name: "Villafrati",
+        value: "villafrati"
+    },
+    {
+        name: "Castellana sicula",
+        value: "castellana+sicula"
+    },
+    {
+        name: "Polizzi Generosa",
+        value: "polizzi+generosa"
+    },
+    {
+        name: "Montemaggiore Belsito",
+        value: "montemaggiore+belsito"
+    },
+    {
+        name: "Petralia Sottana",
+        value: "petralia+sottana"
+    },
+    {
+        name: "Camporeale",
+        value: "camporeale"
+    },
+    {
+        name: "Trappeto",
+        value: "trappeto"
+    },
+    {
+        name: "Castronovo di Sicilia",
+        value: "castronovo+di+sicilia"
+    },
+    {
+        name: "Pollina",
+        value: "pollina"
+    },
+    {
+        name: "Mezzojuso",
+        value: "mezzojuso"
+    },
+    {
+        name: "Sciara",
+        value: "sciara"
+    },
+    {
+        name: "Petralia Sottana",
+        value: "petralia+sottana"
+    },
+    {
+        name: "Chiusa Sclafani",
+        value: "chiusa+sclafani"
+    },
+    {
+        name: "Vicari",
+        value: "vicari"
+    },
+    {
+        name: "Roccapalumba",
+        value: "roccapalumba"
+    },
+    {
+        name: "Giardinello",
+        value: "giardinello"
+    },
+    {
+        name: "Palazzo Adriano",
+        value: "palazzo+adriano"
+    },
+    {
+        name: "Alimena",
+        value: "alimena"
+    },
+    {
+        name: "Ventimiglia di Sicilia",
+        value: "ventimiglia+di+sicilia"
+    },
+    {
+        name: "Baucina",
+        value: "baucina"
+    },
+    {
+        name: "Giuliana",
+        value: "giuliana"
+    },
+    {
+        name: "Geraci Siculo",
+        value: "geraci+siculo"
+    },
+    {
+        name: "Contessa Entellina",
+        value: "contessa+entellina"
+    },
+    {
+        name: "San Mauro Castelverde",
+        value: "san+mauro+castelverde"
+    },
+    {
+        name: "Isnello",
+        value: "isnello"
+    },
+    {
+        name: "Roccamena",
+        value: "roccamena"
+    },
+    {
+        name: "Bompietro",
+        value: "bompietro"
+    },
+    {
+        name: "Ustica",
+        value: "ustica"
+    },
+    {
+        name: "Campofiorito",
+        value: "campofiorito"
+    },
+    {
+        name: "Aliminusa",
+        value: "aliminusa"
+    },
+    {
+        name: "Godrano",
+        value: "godrano"
+    },
+    {
+        name: "Cefalà Diana",
+        value: "cefalà+diana"
+    },
+    {
+        name: "Blufi",
+        value: "blufi"
+    },
+    {
+        name: "Santa Cristina Gela",
+        value: "santa+cristina+gela"
+    },
+    {
+        name: "Gratteri",
+        value: "gratteri"
+    },
+    {
+        name: "Scillato",
+        value: "scillato"
+    },
+    {
+        name: "Campofelice di Fitalia",
+        value: "campofelice+di+fitalia"
+    },
+    {
+        name: "Sclafani Bagni",
+        value: "sclafani+bagni"
+    }
+];
+
+const lang = [
+    {
+        name:"",
+        value:""
+    },
+    {
+        name:"Enter your favorite language!",
+        value:"Enter your favorite language!"
+    },
+    {
+        name:"",
+        value:""
+    },
+    {
+        name: "Italian",
+        value: "it"
+    },
+    {
+        name: "English",
+        value: "en"
+    },
+    {
+        name: "French",
+        value: "fr"
+    },
+    {
+        name: "Spanish",
+        value: "es"
+    }
+];
+
+const traductionText =[
+    {
+        value: "it",
+        p1it: "Il meteo oggi prevede",
+        p2it: "Le nuvole sono al",
+        p3it: "La velocità del vento è"
+    },
+    {
+        value: "en",
+        p1en: "The weather forecast today",
+        p2en:"The clouds are at",
+        p3en:"Wind speed is"
+    },
+    {
+        value: "fr",
+        p1fr:"La météo d'aujourd'hui",
+        p2fr:"Les nuages ​​sont al",
+        p3fr:"La vitesse du vent est"
+    },
+    {
+        value: "es",
+        p1es:"El pronóstico del tiempo hoy",
+        p2es:"Las nubes son todas",
+        p3es:"La velocidad del viento es"
+    }
+];
+
+export {cities, lang, traductionText};
